7f0dfff4112ad5a122f523ec7414099d5b233f36
Sushant11 <sushantchitrakar@gmail.com>
2019-10-06 14:21:17 +0545

    loader

--- client/src/containers/App/AsyncComponent.js
+++ client/src/containers/App/AsyncComponent.js
@@ -1,6 +1,7 @@
 import loadable from 'react-loadable';
+import LoadingComponent from '../../components/Loading'
 
 export const AsyncDashboard = loadable({
     loader: () => import('./../Dashboard'),
-    loading: true
+    loading: LoadingComponent
 });
